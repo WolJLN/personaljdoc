8859e9950a64fc1886d69730989d034c37c8fdf9
WolJLN <lswangling@hotmail.com>
2020-02-28 10:19:07 +0800

    Add files via upload

---
+++ src/main/java/org/word/model/ModelAttr.java
@@ -0,0 +1,46 @@
+package org.word.model;
+
+import lombok.Data;
+import lombok.ToString;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 返回属性
+ *
+ * @author kevin
+ */
+@Data
+@ToString
+public class ModelAttr implements Serializable {
+
+    private static final long serialVersionUID = -4074067438450613643L;
+
+    /**
+     * 类名
+     */
+    private String className = StringUtils.EMPTY;
+    /**
+     * 属性名
+     */
+    private String name = StringUtils.EMPTY;
+    /**
+     * 类型
+     */
+    private String type = StringUtils.EMPTY;
+    /**
+     * 属性描述
+     */
+    private String description;
+    /**
+     * 嵌套连接
+     */
+    private String ref;
+    /**
+     * 嵌套属性列表
+     */
+    private List<ModelAttr> properties = new ArrayList<>();
+}
